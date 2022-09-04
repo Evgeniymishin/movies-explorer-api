0c2868e3ace2bafa5870d7a468a8745276644686
Evgeniy Mishin <dubreak@yandex.ru>
2022-09-04 14:45:35 +0300

    feat: улучшена безопасность API

--- app.js
+++ app.js
@@ -3,24 +3,33 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-// const cors = require('cors');
+const cors = require('cors');
+const helmet = require('helmet');
 const { PORT = 3000 } = require('./utils/constants');
 const errorHandler = require('./middlewares/error-handler');
 const routes = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const limiter = require('./middlewares/rate-limiter');
 
 require('dotenv').config();
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+mongoose.connect('mongodb://localhost:27017/moviesdb', {
   useNewUrlParser: true,
 });
 
+app.use(limiter);
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-// app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000',
+    'https://evgeniymishin.moviesexplorer.nomorepartiesxyz.ru',
+    'http://evgeniymishin.moviesexplorer.nomorepartiesxyz.ru'],
+  credentials: true,
+}));
 app.use(requestLogger);
 app.use(routes);
 app.use(errorLogger);
